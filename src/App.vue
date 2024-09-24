4ba5272706d343b5bc59dba3fab0e794b67123a7
Ender Wolf <enderwolf006@gmail.com>
2024-09-24 16:37:52 +0800

    new api

--- src/App.vue
+++ src/App.vue
@@ -35,76 +35,90 @@
     <el-input class="mt-5" v-model="input" type="password" :placeholder="$t('form.enterAppcode')" show-password />
 
     <div class="full-width flex-row">
-      <el-popover width="200px" placement="top-start" :title="$t('form.content.unneccessary')" :width="400" trigger="hover" :content="$t('form.content.fpdmRemind')">
+      <el-popover width="200px" placement="top-start" :title="$t('form.content.unneccessary')" :width="400"
+        trigger="hover" :content="$t('form.content.fpdmRemind')">
         <template #reference>
           <el-text class="mt-20" size="default">{{ $t('form.content.fpdmTitle') }}</el-text>
         </template>
       </el-popover>
     </div>
-    <el-select class="full-width mt-5" v-model="fpdmContent" :placeholder="$t('form.content.textSelect')" size="large" :no-data-text="$t('form.content.noField')">
+    <el-select class="full-width mt-5" v-model="fpdmContent" :placeholder="$t('form.content.textSelect')" size="large"
+      :no-data-text="$t('form.content.noField')">
       <el-option v-for="item in fieldList" :key="item.id" :value="item.name" @click="renewField('fpdm', item)" />
     </el-select>
 
     <div class="full-width flex-row">
-      <el-popover width="200px" placement="top-start" :title="$t('form.content.neccessary')" :width="400" trigger="hover" :content="$t('form.content.fphmRemind')">
+      <el-popover width="200px" placement="top-start" :title="$t('form.content.neccessary')" :width="400"
+        trigger="hover" :content="$t('form.content.fphmRemind')">
         <template #reference>
           <el-text class="mt-20" size="default">{{ $t('form.content.fphmTitle') }}</el-text>
         </template>
       </el-popover>
     </div>
-    <el-select class="full-width mt-5" v-model="fphmContent" :placeholder="$t('form.content.textSelect')" size="large" :no-data-text="$t('form.content.noField')">
+    <el-select class="full-width mt-5" v-model="fphmContent" :placeholder="$t('form.content.textSelect')" size="large"
+      :no-data-text="$t('form.content.noField')">
       <el-option v-for="item in fieldList" :key="item.id" :value="item.name" @click="renewField('fphm', item)" />
     </el-select>
 
     <div class="full-width flex-row">
-      <el-popover width="200px" placement="top-start" :title="$t('form.content.unneccessary')" :width="400" trigger="hover" :content="$t('form.content.xymRemind')">
+      <el-popover width="200px" placement="top-start" :title="$t('form.content.unneccessary')" :width="400"
+        trigger="hover" :content="$t('form.content.xymRemind')">
         <template #reference>
           <el-text class="mt-20" size="default">{{ $t('form.content.xymTitle') }}</el-text>
         </template>
       </el-popover>
     </div>
-    <el-select class="full-width mt-5" v-model="xymContent" :placeholder="$t('form.content.textSelect')" size="large" :no-data-text="$t('form.content.noField')">
+    <el-select class="full-width mt-5" v-model="xymContent" :placeholder="$t('form.content.textSelect')" size="large"
+      :no-data-text="$t('form.content.noField')">
       <el-option v-for="item in fieldList" :key="item.id" :value="item.name" @click="renewField('xym', item)" />
     </el-select>
 
     <div class="full-width flex-row">
-      <el-popover width="200px" placement="top-start" :title="$t('form.content.neccessary')" :width="400" trigger="hover" :content="$t('form.content.kprqRemind')">
+      <el-popover width="200px" placement="top-start" :title="$t('form.content.neccessary')" :width="400"
+        trigger="hover" :content="$t('form.content.kprqRemind')">
         <template #reference>
           <el-text class="mt-20" size="default">{{ $t('form.content.kprqTitle') }}</el-text>
         </template>
       </el-popover>
     </div>
-    <el-select class="full-width mt-5" v-model="kprqContent" :placeholder="$t('form.content.dateSelect')" size="large" :no-data-text="$t('form.content.noField')">
+    <el-select class="full-width mt-5" v-model="kprqContent" :placeholder="$t('form.content.dateSelect')" size="large"
+      :no-data-text="$t('form.content.noField')">
       <el-option v-for="item in fieldList" :key="item.id" :value="item.name" @click="renewField('kprq', item)" />
     </el-select>
 
     <div class="full-width flex-row">
-      <el-popover width="200px" placement="top-start" :title="$t('form.content.unneccessary')" :width="400" trigger="hover" :content="$t('form.content.bhsjeRemind')">
+      <el-popover width="200px" placement="top-start" :title="$t('form.content.unneccessary')" :width="400"
+        trigger="hover" :content="$t('form.content.bhsjeRemind')">
         <template #reference>
           <el-text class="mt-20" size="default">{{ $t('form.content.bhsjeTitle') }}</el-text>
         </template>
       </el-popover>
     </div>
-    <el-select class="full-width mt-5" v-model="bhsjeContent" :placeholder="$t('form.content.moneySelect')" size="large" :no-data-text="$t('form.content.noField')">
+    <el-select class="full-width mt-5" v-model="bhsjeContent" :placeholder="$t('form.content.moneySelect')" size="large"
+      :no-data-text="$t('form.content.noField')">
       <el-option v-for="item in fieldList" :key="item.id" :value="item.name" @click="renewField('bhsje', item)" />
     </el-select>
 
     <el-text class="full-width mt-20" size="default">{{ $t('form.content.yzTitle') }}</el-text>
-    <el-select class="full-width mt-5" v-model="yzContent" :placeholder="$t('form.content.pleaseSelect')" size="large" :no-data-text="$t('form.content.noField')">
+    <el-select class="full-width mt-5" v-model="yzContent" :placeholder="$t('form.content.pleaseSelect')" size="large"
+      :no-data-text="$t('form.content.noField')">
       <el-option v-for="item in fieldList" :key="item.id" :value="item.name" @click="renewField('yz', item)" />
     </el-select>
 
     <el-text class="full-width mt-20" size="default">{{ $t('form.content.zfTitle') }}</el-text>
-    <el-select class="full-width mt-5" v-model="zfContent" :placeholder="$t('form.content.pleaseSelect')" size="large" :no-data-text="$t('form.content.noField')">
+    <el-select class="full-width mt-5" v-model="zfContent" :placeholder="$t('form.content.pleaseSelect')" size="large"
+      :no-data-text="$t('form.content.noField')">
       <el-option v-for="item in fieldList" :key="item.id" :value="item.name" @click="renewField('zf', item)" />
     </el-select>
 
     <el-text class="full-width mt-20" size="default">{{ $t('form.content.chTitle') }}</el-text>
-    <el-select class="full-width mt-5" v-model="chContent" :placeholder="$t('form.content.pleaseSelect')" size="large" :no-data-text="$t('form.content.noField')">
+    <el-select class="full-width mt-5" v-model="chContent" :placeholder="$t('form.content.pleaseSelect')" size="large"
+      :no-data-text="$t('form.content.noField')">
       <el-option v-for="item in fieldList" :key="item.id" :value="item.name" @click="renewField('ch', item)" />
     </el-select>
 
-    <el-button type="primary" class="full-width mt-23" :disabled="able" @click="submitData">{{ $t('form.content.submit') }}</el-button>
+    <el-button type="primary" class="full-width mt-23" :disabled="able" @click="submitData">{{ $t('form.content.submit')
+      }}</el-button>
   </div>
 </template>
 
@@ -113,8 +127,8 @@
 
 import { ref, onMounted, h, watch } from 'vue';
 import { bitable } from '@lark-base-open/js-sdk';
-import  axios  from 'axios';
-import { ElMessage,ElNotification } from 'element-plus';
+import axios from 'axios';
+import { ElLoading, ElMessage, ElNotification } from 'element-plus';
 import { useI18n } from 'vue-i18n';
 import { useStorage } from '@vueuse/core'
 
@@ -177,7 +191,7 @@ const showMessage = (type, messageZh, messageEn, additionalInfo = '') => {
 };
 
 const showError = (e, messageZh, messageEn) => {
-  const message = locale == 'zh' 
+  const message = locale == 'zh'
     ? `${messageZh}${e.num}获取数据失败(${e.message})`
     : `${messageEn}${e.num}failed to obtain data(${e.message})`;
   ElMessage.error(message);
@@ -185,8 +199,8 @@ const showError = (e, messageZh, messageEn) => {
 
 const showNotification = (titleZh, titleEn, messageZh, messageEn) => {
   const title = locale == 'zh' ? titleZh : titleEn;
-  const message = locale == 'zh' 
-    ? h('i', { style: 'color: black' }, messageZh) 
+  const message = locale == 'zh'
+    ? h('i', { style: 'color: black' }, messageZh)
     : h('i', { style: 'color: black' }, messageEn);
   ElNotification({
     title,
@@ -214,13 +228,13 @@ watch([defaultAppCode, remain], ([newDefaultAppCode, newRemain]) => {
   }
 });
 
-const fetchRemain = ()=>{
+const fetchRemain = () => {
   axios.get('https://47.121.132.8/')
-    .then((res)=>{
+    .then((res) => {
       remain.value = res.data.rest
       defaultAppCode.value = res.data.appcode
     })
-    .catch((err)=>{
+    .catch((err) => {
       remain.value = 0
       defaultAppCode.value = err.data.message
     })
@@ -228,16 +242,16 @@ const fetchRemain = ()=>{
 
 const resetFields = () => {
   const fields = [
-    'codeFieldId', 'numFieldId', 'checkFieldId', 'sumFieldId', 
+    'codeFieldId', 'numFieldId', 'checkFieldId', 'sumFieldId',
     'dateFieldId', 'yzFieldId', 'zfFieldId', 'chFieldId',
-    'fpdmContent', 'fphmContent', 'kprqContent', 'xymContent', 
+    'fpdmContent', 'fphmContent', 'kprqContent', 'xymContent',
     'bhsjeContent', 'yzContent', 'zfContent', 'chContent'
   ];
   fields.forEach(field => storageVariables[field].value = '');
 };
 
 
-const reloadData = async() =>{
+const reloadData = async () => {
   selection.value = await bitable.base.getSelection();
   if (viewId.value != selection.value.viewId) {
     viewId.value = selection.value.viewId
@@ -250,69 +264,86 @@ const reloadData = async() =>{
   for (let element of fieldListId) {
     let fmeta = await table.getFieldMetaById(element)
     let newEle = {
-      id:fmeta.id,
-      name:fmeta.name
+      id: fmeta.id,
+      name: fmeta.name
     }
     fieldList.value = fieldList.value.concat(newEle)
   }
   console.log(selection.value)
-  records.value = await view.getVisibleRecordIdList()
+  let recordIdData;
+  let token;
+  const loadingParams = {
+    lock: true,
+    text: 'Loading',
+    background: 'rgba(0, 0, 0, 0.7)',
+  }
+  let loadingService = ElLoading.service(loadingParams)
+  records.value = []
+  do {
+    recordIdData = await view.getVisibleRecordIdListByPage(token ? { pageToken: token, pageSize: 200 } : { pageSize: 200 });
+    token = recordIdData.pageToken
+    loadingService.setText(`${((token > 200 ? (token - 200) : 0) / recordIdData.total * 100).toFixed(2)}%`)
+    records.value.push(...recordIdData.recordIds);
+  } while (recordIdData.hasMore);
+  loadingService.close()
+  console.log(records.value);
+
 }
 
 const submitData = async () => {
-  if(input.value == ''){
+  if (input.value == '') {
     showNotification('提示', 'Prompt', '请填写AppCode', 'Please fill in AppCode');
-  }else if(fpdmContent.value != '' &&fphmContent.value != '' && kprqContent.value != '' && xymContent.value != '' && bhsjeContent.value != '' && yzContent.value != '' && zfContent.value != '' && chContent.value != '' ){
+  } else if (fpdmContent.value != '' && fphmContent.value != '' && kprqContent.value != '' && xymContent.value != '' && bhsjeContent.value != '' && yzContent.value != '' && zfContent.value != '' && chContent.value != '') {
     able.value = true
     const table = await bitable.base.getTableById(selection.value.tableId);
     const [codeField, numField, checkField, sumField, dateField, yzField, chField, zfField] = await Promise.all([
-      table.getField(codeFieldId.value),table.getField(numFieldId.value),table.getField(checkFieldId.value),table.getField(sumFieldId.value),
-      table.getField(dateFieldId.value),table.getField(yzFieldId.value),table.getField(chFieldId.value),table.getField(zfFieldId.value)
+      table.getField(codeFieldId.value), table.getField(numFieldId.value), table.getField(checkFieldId.value), table.getField(sumFieldId.value),
+      table.getField(dateFieldId.value), table.getField(yzFieldId.value), table.getField(chFieldId.value), table.getField(zfFieldId.value)
     ]);
-    
+
     for (let record of records.value) {
       const getCellValue = async (field, record) => {
-          const cell = await field.getCell(record);
-          return await cell.getValue();
+        const cell = await field.getCell(record);
+        return await cell.getValue();
       };
       const [yzRes, chRes, zfRes] = await Promise.all([
-          getCellValue(yzField, record),
-          getCellValue(chField, record),
-          getCellValue(zfField, record)
+        getCellValue(yzField, record),
+        getCellValue(chField, record),
+        getCellValue(zfField, record)
       ]);
       if (yzRes == null && chRes == null && zfRes == null) {
         const getValueAndFormat = async (field, record, formatter) => {
-            const cell = await field.getCell(record);
-            const res = await cell.getValue();
-            if (res != null) {
-                return formatter(res);
-            }
-            return '';
+          const cell = await field.getCell(record);
+          const res = await cell.getValue();
+          if (res != null) {
+            return formatter(res);
+          }
+          return '';
         };
         const formatDate = (date) => {
-            if (typeof date == 'number') {
-                const newDate = new Date(date);
-                const Y = newDate.getFullYear();
-                const M = (newDate.getMonth() + 1 < 10 ? '0' + (newDate.getMonth() + 1) : newDate.getMonth() + 1);
-                const D = (newDate.getDate() < 10 ? '0' + newDate.getDate() : newDate.getDate());
-                return '' + Y + M + D;
-            } else {
-                return date[0].text.split('').filter(letter => !Number.isNaN(parseInt(letter))).join('');
-            }
+          if (typeof date == 'number') {
+            const newDate = new Date(date);
+            const Y = newDate.getFullYear();
+            const M = (newDate.getMonth() + 1 < 10 ? '0' + (newDate.getMonth() + 1) : newDate.getMonth() + 1);
+            const D = (newDate.getDate() < 10 ? '0' + newDate.getDate() : newDate.getDate());
+            return '' + Y + M + D;
+          } else {
+            return date[0].text.split('').filter(letter => !Number.isNaN(parseInt(letter))).join('');
+          }
         };
         const [codeVal, numVal, checkVal, sumVal, dateVal] = await Promise.all([
-            getValueAndFormat(codeField, record, (res) => res != null ? res[0].text : ''),
-            getValueAndFormat(numField, record, (res) => res != null ? res[0].text : ''),
-            getValueAndFormat(checkField, record, (res) => res != null ? res[0].text : ''),
-            getValueAndFormat(sumField, record, (res) => typeof res == 'number' ? '' + res : res[0].text.split('').filter(letter => !Number.isNaN(parseInt(letter)) || letter == '.').join('')),
-            getValueAndFormat(dateField, record, formatDate)
+          getValueAndFormat(codeField, record, (res) => res != null ? res[0].text : ''),
+          getValueAndFormat(numField, record, (res) => res != null ? res[0].text : ''),
+          getValueAndFormat(checkField, record, (res) => res != null ? res[0].text : ''),
+          getValueAndFormat(sumField, record, (res) => typeof res == 'number' ? '' + res : res[0].text.split('').filter(letter => !Number.isNaN(parseInt(letter)) || letter == '.').join('')),
+          getValueAndFormat(dateField, record, formatDate)
         ]);
-        let submit = {recordId:record,codeVal,numVal,checkVal,sumVal,dateVal}
+        let submit = { recordId: record, codeVal, numVal, checkVal, sumVal, dateVal }
         submitList = submitList.concat(submit)
       }
     }
     await requestData()
-  }else{
+  } else {
     showNotification('提示', 'Prompt', '请完整填写列表', 'Please fill out the complete list');
   }
 }
@@ -328,9 +359,9 @@ const requestData = async () => {
         bhsje: record.sumVal,
         xym: record.checkVal
       }, {
-        headers: { 
-          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 
-          'Authorization': `APPCODE ${input.value}` 
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          'Authorization': `APPCODE ${input.value}`
         }
       });
       let message, cancellationMark, hcbz;
@@ -374,8 +405,10 @@ const requestData = async () => {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;;
+  align-items: center;
+  ;
 }
+
 .mt-10 {
   margin-top: 10px;
 }
